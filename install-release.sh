f172e6c581cfbbc4d87ee95f1f942bc7dfb34960
lipeng0820 <lipeng0820@qq.com>
2021-07-10 23:07:55 +0800

    Update install-release.sh

--- install-release.sh
+++ install-release.sh
@@ -96,7 +96,7 @@ sysArch(){
     elif [[ "$ARCH" == *"armv7"* ]] || [[ "$ARCH" == "armv6l" ]]; then
         VDIS="arm"
     elif [[ "$ARCH" == *"armv8"* ]] || [[ "$ARCH" == "aarch64" ]]; then
-        VDIS="arm64"
+        VDIS="64"
     elif [[ "$ARCH" == *"mips64le"* ]]; then
         VDIS="mips64le"
     elif [[ "$ARCH" == *"mips64"* ]]; then
